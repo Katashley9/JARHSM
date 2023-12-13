f6b36645dd08235d66eb69d70e5291e967307e69
Katashley9 <144281115+Katashley9@users.noreply.github.com>
2023-12-12 21:10:03 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@ const config = {
   port: 3000,
   jwtSecret: "YOUR_secret_key",
   mongoUri:
-    "mongodb+srv://katialyashleymetsanou:GqrI7GfPvn4Wb3gq@group6.j2dkqni.mongodb.net/?retryWrites=true&w=majority",
+   "mongodb+srv://katialyashleymetsanou:GqrI7GfPvn4Wb3gq@group6.j2dkqni.mongodb.net/?retryWrites=true&w=majority",
 };
 export default config;
